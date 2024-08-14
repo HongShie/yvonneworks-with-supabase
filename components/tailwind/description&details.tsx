70df6243d7219dfee4352ee6b18533d68848539e
HongShie <wonghongshie0816@e.newera.edu.my>
2024-08-14 21:21:04 +0800

    modified:   components/tailwind/description&details.tsx

--- components/tailwind/description&details.tsx
+++ components/tailwind/description&details.tsx
@@ -5,33 +5,36 @@ const cohere = new CohereClient({
   });
 
 export default function DescriptionAndDetails({artName}:{artName: React.ReactNode}) {
-    console.log(artName)
-    const fetchDescription = (async () => {
-        const response = await cohere.chat({
-          message: artName + " is the name of the artpiece. Write a single paragraph about this original artpeice with an artistic perspective and must be less than 45 words"
-        });
+  console.log(artName)
+  const newDescription = (async () => {
+    const response = await cohere.chat({
+      message: artName + " is the name of the artpiece. Write a single paragraph about this original artpeice with an artistic perspective and must be less than 45 words"
+    });
 
-        console.log(response.text)
-        return (response.text)
-      })();
+    console.log(response.text);
+    return (response.text);
+  })();
+    const fetchDescription = newDescription;
 
-      const fetchHighlight = (async () => {
-        const response = await cohere.chat({
-          message: "Write 2 highlights about the artpiece called " + artName +  " as well as 2 highlights about the material of the artpiece . Each highlight must be less than 10 words. Replace the numbering system with comma. Don't mention anything with the artpiece's name"
-        });
+  const newHighlight = (async () => {
+    const response = await cohere.chat({
+      message: "Write 2 highlights about the artpiece called " + artName + " as well as 2 highlights about the material of the artpiece . Each highlight must be less than 10 words. Replace the numbering system with comma. Don't mention anything with the artpiece's name"
+    });
 
-        console.log(response.text)
-        return (response.text)
-      })();
+    console.log(response.text);
+    return (response.text);
+  })();
+      const fetchHighlight = newHighlight;
 
-      const fetchDetail = (async () => {
-        const response = await cohere.chat({
-          message: "Write about how " + artName +  ", the artpiece has been precisely and carefully detailed and describe what artistic material has been used and must less than 50 words."
-        });
+  const newDetail = (async () => {
+    const response = await cohere.chat({
+      message: "Write about how " + artName + ", the artpiece has been precisely and carefully detailed and describe what artistic material has been used and must less than 50 words."
+    });
 
-        console.log(response.text)
-        return (response.text)
-      })();
+    console.log(response.text);
+    return (response.text);
+  })();
+      const fetchDetail = newDetail;
     return (
         <div className="py-10 lg:col-span-2 lg:col-start-1 lg:border-r lg:border-gray-200 lg:pb-16 lg:pr-8 lg:pt-6">
             {/* Description and details */}
