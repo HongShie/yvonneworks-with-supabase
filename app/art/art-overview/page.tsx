0337df34a59c8391708898d06f7e5a4deea25c60
HongShie <wonghongshie0816@e.newera.edu.my>
2024-08-15 03:31:30 +0800

    modified:   app/art/art-overview/page.tsx

--- app/art/art-overview/page.tsx
+++ app/art/art-overview/page.tsx
@@ -5,15 +5,9 @@ import { Suspense } from 'react';
 
 export default async function Example() {
   const searchParams = useSearchParams();
-  const artName =  searchParams.get("name");
-  const artPrice = searchParams.get("price") ;
-  const artLink =  searchParams.get("artlink");
-  const artpv1 =   searchParams.get("artpv1") ;
-  const artpv2 =   searchParams.get("artpv2") ;
-  const artpv3 =   searchParams.get("artpv3") ;
   return (
     <Suspense>
-      <ProductOverview artName={artName} artPrice={artPrice} artLink={artLink} artpv1={artpv1} artpv2={artpv2} artpv3={artpv3}></ProductOverview>
+      <ProductOverview artName={searchParams.get("name")} artPrice={searchParams.get("price")} artLink={searchParams.get("artlink")} artpv1={searchParams.get("artpv1")} artpv2={searchParams.get("artpv2")} artpv3={searchParams.get("artpv3")}></ProductOverview>
     </Suspense>
   )
 }
