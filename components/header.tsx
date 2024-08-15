7c1da2a31d2d6ce849617669f1415356d6acd307
HongShie <wonghongshie0816@e.newera.edu.my>
2024-08-15 20:54:21 +0800

    modified:   components/header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -18,7 +18,7 @@ export default function Header() {
         <header className="absolute inset-x-0 top-0 z-50">
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.0">
+            <a href="/" className="-m-1.5 p-1.0">
               <span className="sr-only">Your Company</span>
               <img alt="logo" src={logo.src} className="h-12 w-auto"/>
             </a>
@@ -43,7 +43,7 @@ export default function Header() {
           <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
+              <a href="/" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
                 <img alt="" src={logo.src} className="h-12 w-auto"/>
               </a>
