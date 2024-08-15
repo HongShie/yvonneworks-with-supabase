12003834a620aeb2bfd706b8b782a12b651ffc31
HongShie <wonghongshie0816@e.newera.edu.my>
2024-08-15 13:10:51 +0800

    modified:   app/art/art-overview/page.tsx

--- app/art/art-overview/page.tsx
+++ app/art/art-overview/page.tsx
@@ -3,11 +3,24 @@ import ProductOverview from '@/components/tailwind/productOverview';
 import { useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
 
-export default async function Example() {
+function Search(){
   const searchParams = useSearchParams();
+  const artName = searchParams.get("name") ?? "";
+  const artPrice = searchParams.get("price") ?? "";
+  const artLink = searchParams.get("artlink") ?? "";
+  const artpv1 = searchParams.get("artpv1") ?? "";
+  const artpv2 = searchParams.get("artpv2") ?? "";
+  const artpv3 = searchParams.get("artpv3") ?? "";
+  return {artName, artPrice, artLink, artpv1, artpv2, artpv3};
+}
+
+
+export default async function Example() {
+  const {artName, artPrice, artLink, artpv1, artpv2, artpv3} = Search();
+
   return (
     <Suspense>
-      <ProductOverview artName={searchParams.get("name")} artPrice={searchParams.get("price")} artLink={searchParams.get("artlink")} artpv1={searchParams.get("artpv1")} artpv2={searchParams.get("artpv2")} artpv3={searchParams.get("artpv3")}></ProductOverview>
+      <ProductOverview artName={artName} artPrice={artPrice} artLink={artLink} artpv1={artpv1} artpv2={artpv2} artpv3={artpv3}></ProductOverview>
     </Suspense>
   )
 }
