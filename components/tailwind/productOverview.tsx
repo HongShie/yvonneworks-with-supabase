9aa220d5a19dc8654601f08bef15681e78489acc
HongShie <wonghongshie0816@e.newera.edu.my>
2024-08-17 16:47:39 +0800

    modified:   components/tailwind/productOverview.tsx

--- components/tailwind/productOverview.tsx
+++ components/tailwind/productOverview.tsx
@@ -220,12 +220,12 @@ export default function productOverview(
                 type="submit"
                 className="mt-10 flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               >
-                Buy Now
+                Buy
               </button>
             </form>
           </div>
 
-          {/* <DescriptionAndDetails artName={artName}></DescriptionAndDetails> */}
+          <DescriptionAndDetails artName={artName}></DescriptionAndDetails>
         </div>
       </div>
     </div>
